435b35e2642437cd1643c97c0b3b46514703c0c2
Mitko Bachvarov <mitko.bachvarov@socialsweethearts.de>
2018-07-09 15:10:14 +0100

    adding a file to test auc

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const auth = require('./routes/auth');
+const user = require('./routes/user');
+const index = require('./routes/index');
+
+const passport = require('./passport');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','pug');
+
+//uncomment the following after placing your favicon in /public
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/auth', auth);
+app.use('/user', passport.authenticate('jwt', {session: false}, user));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+    let err = new Error('Not found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use((err,req,res,next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render error page
+    res.status(err.status || 500);
+    res.render(error);
+});
+
+
+module.exports = app;
+
+
